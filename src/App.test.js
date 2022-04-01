0b78fcce57fe0e7060b47b8442d7c643606eff1a
Thacher Hussain <thacher.hussain@gmail.com>
2022-03-31 22:42:24 -0400

    survey app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+import App from './App'
+
+jest.useFakeTimers()
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  // const linkElement = screen.getByText(/learn react/i)
+  // expect(linkElement).toBeInTheDocument()
+})
+
+describe('Example', () => {
+  it('should complete page 1', async () => {
+    render(<App />)
+
+    act(() => {
+      jest.runAllTimers()
+    })
+
+    const name = screen.queryByPlaceholderText('Name (optional)')
+    const email = screen.queryByPlaceholderText('Email (optional)')
+
+    fireEvent.change(name, { target: { value: 'Danger Mouse' } })
+    fireEvent.change(email, { target: { value: 'dm@adventures.com' } })
+
+    act(() => {
+      screen.getByText('Next').click()
+    })
+
+    await waitForElementToBeRemoved(name)
+  })
+
+  // it('should complete page 2 ', async () => {
+  //   render(<App initialStep={2} />)
+  // })
+})
